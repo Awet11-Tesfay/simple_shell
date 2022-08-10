3572bbec6feb44dcaa2a1864873d5d4a3d498c98
Awet11_Tesfay <hailegb2021@gmail.com>
2022-08-10 16:25:19 -0700

    Task for two

--- get_help.c
+++ get_help.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_help - function that retrieves help messages according builtin
+ * get_help - function that retrieves help messages according builtin fun
  * @datash: data structure (args and input)
  * Return: Return 0
 */
